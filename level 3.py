8d3dc183cb8915eb5813438137408d0351761b9a
anigigauri37 <anigigauri37@gmail.com>
2024-05-21 21:37:14 +0400

    first commit

---
+++ level 3.py
@@ -0,0 +1,20 @@
+Name = 'anni'
+surname = 'gigauri'
+hobby = 'cekva'
+country = 'saqartvelo'
+
+print(Name + " " + surname + " " + hobby + " " + country )
+
+
+
+print(10 + 200)
+print(5 - 10)
+print(1000 // 2 )
+print(785 + 274)
+print(58 * 2)
+print(86472 // 3)
+print(5000 - 8000)
+print(2 - 0)
+print( 876 * 7)
+print(2050 // 5)
+
