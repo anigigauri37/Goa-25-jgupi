0ccc48a303a9e359057ce4ed477c22a6375257a1
anigigauri37 <anigigauri37@gmail.com>
2024-06-07 17:42:44 +0400

    commit

---
+++ day09/homework/day09.py
@@ -0,0 +1,10 @@
+name = input("what is your name: ")
+last_name = input("what is your last name: ")
+age = input("what is your age: ")
+country = input("which country do you live: ")
+
+print("my name is ", name, last_name, "i am ", age, " years old" " and i live in ", country)
+
+
+
+
