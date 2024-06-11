ae2fee80fd31b80e648bf5a526f761954586e226
anigigauri37 <anigigauri37@gmail.com>
2024-06-11 15:15:51 +0400

    commit

---
+++ day12/homework/day12.py
@@ -0,0 +1,31 @@
+number1 = int(input("enter your number"))
+number2 = int(input("enter your number"))
+print(number1 + number2)
+
+#დაწერეთ პითონის პროგრამა რომელიც ითვლის მარტკუტხედის ფარტობს
+
+width = int(input("enter your numbet"))
+height = int(input("enter your number"))
+print(width * height)
+
+#დაწერეთ პიტონის პროგრამა რომელიც მოუწოდებს მომხმარებელს შეიყვანოს რიცხვი და შემდეგ დაბეჭდოს ამ რიცხვის ორმაგი
+
+number1 = int(input("enter yout number"))
+number2 = int(input("enter your number"))
+print(20 + 20)
+
+#დაწერეთ პიტონის პროგრამა რომელიც ითვლის მართკუთხედის პერიმეტრს
+
+
+length = float(input("შეიყვანეთ მართკუთხედის სიგრძე: "))
+width = float(input("შეიყვანეთ მართკუთხედის სიგანე: "))
+
+perimeter = 2 * (length + width)
+
+print(f" {perimeter}")
+
+#დაწერეთ პითონის პროგრამა რომელიც მოუწოდებს მომხმარებელს შეიყვანოს რიცხვი და შემდეგ დაბეჭდოს ამ რიცხვის კვადრატი
+
+number = int(input("Enter your number"))
+print(5 ** 5)
+
