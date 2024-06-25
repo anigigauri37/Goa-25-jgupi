935459d16154444c45b4472571168344dd0f854c
anigigauri37 <anigigauri37@gmail.com>
2024-06-25 16:09:45 +0400

    commit

---
+++ day13/homework/13.py
@@ -0,0 +1,56 @@
+#გააკეთეთ 10 მაგალითი ლოგიკურ ოპერატორებზე(True and False)
+
+
+num1 = int(input("enter: "))
+num2 = int(input("enter: "))
+print(num1 < num2)
+
+num1 = int(input("enter: "))
+num2 = int(input("enter: "))
+print(num1 > num2)
+
+num1 = int(input("enter: "))
+num2 = int(input("enter: "))
+print(num1 < num2)
+
+num1 = int(input("enter: "))
+num2 = int(input("enter: "))
+print(num1 > num2)
+
+num6 = int(input("enter: "))
+num2 = int(input("enter: "))
+print(num1 > num2)
+
+num10 = int(input("enter: "))
+num3 = int(input("enter: "))
+print(num1 < num2)
+
+num8 = int(input("enter: "))
+num9 = int(input("enter: "))
+print(num1 > num2)
+
+num9 = int(input("enter: "))
+num7 = int(input("enter: "))
+print(num1 < num2)
+
+num2 = int(input("enter: "))
+num4 = int(input("enter: "))
+print(num1 > num2)
+
+num5 = int(input("enter: "))
+num2 = int(input("enter: "))
+print(num1 < num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
